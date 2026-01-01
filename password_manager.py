186b4b9865ec21b3816b1d3d99e28bfaac8afb72
Andrei Dina <andreidina04@gmail.com>
2026-01-01 14:09:05 +0200

    Improve PIN verification and UI elements
    Enhanced PIN verification process with attempt tracking and improved UI elements. Updated button styles and added functionality for saving and viewing passwords.

--- password_manager.py
+++ password_manager.py
@@ -1,3 +1,4 @@
+import customtkinter
 import customtkinter as ctk
 from cryptography.fernet import Fernet
 from pin import setup_pin, hash_pin, PIN_FILE
@@ -32,6 +33,7 @@ def show_frame(frame):
         f.pack_forget()
     frame.pack(expand=True, fill="both")
 
+ctk.CTkLabel(pin_frame, text="Password Manager", font=("Arial", 20, "bold")).pack(pady=20)
 pin_entry = ctk.CTkEntry(pin_frame, placeholder_text="Enter PIN", show="*", text_color="white", width=300, height=50, corner_radius= 20, justify="center", fg_color="#212946",border_width=1, border_color="#FFFFFF")
 pin_entry.pack(pady=20)
 
@@ -39,23 +41,29 @@ pin_result = ctk.CTkLabel(pin_frame, text="")
 pin_result.pack()
 
 # Verify entered PIN against stored hash
-def verify_pin():
+attempts = 4
+def verify_pin(event=None):
+    global attempts
     with open(PIN_FILE) as f:
         saved = f.read()
-    if hash_pin(pin_entry.get()) == saved:
+    if hash_pin(pin_entry.get()) == saved: # Checking if the entered pin is the same as the saved one
         show_frame(menu_frame)
     else:
-        pin_result.configure(text="Wrong PIN. Try again!", text_color="red", font=("Arial", 14, "bold"))
+            attempts -= 1
+            if attempts > 0:
+                pin_result.configure(text=f"Wrong PIN. Attempts left: {attempts}", text_color="red", font=("Arial", 14, "bold"))
+            else:
+                pin_result.configure(text=f"{attempts} attempts left! Closing the app...",text_color="red", font=("Arial", 14, "bold"))
+                app.after(800, exit)
 
 ctk.CTkButton(pin_frame,text="Verify", command=verify_pin, fg_color="#FFFFFF", text_color="#141936", corner_radius=50, hover_color="#ADADAD", width=200, height=40, font=("Arial", 15, "bold")).pack(pady=10)
+ctk.CTkLabel(menu_frame, text="Password Manager", font=("Arial", 20, "bold")).pack(pady=20)
+ctk.CTkButton(menu_frame, text="Add Password", command=lambda: show_frame(add_frame), fg_color="#FFFFFF", text_color="#141936", corner_radius=50, hover_color="#ADADAD", width=200, height=40, font=("Arial", 15, "bold")).pack(pady=10)
+ctk.CTkButton(menu_frame, text="View Passwords", command=lambda: show_frame(view_frame), fg_color="#FFFFFF", text_color="#141936", corner_radius=50, hover_color="#ADADAD", width=200, height=40, font=("Arial", 15, "bold")).pack(pady=10)
 
-ctk.CTkLabel(menu_frame, text="Password Manager", font=("Arial", 20)).pack(pady=20)
-ctk.CTkButton(menu_frame, text="Add Password", command=lambda: show_frame(add_frame)).pack(pady=10)
-ctk.CTkButton(menu_frame, text="View Passwords", command=lambda: show_frame(view_frame)).pack(pady=10)
-
-app_entry = ctk.CTkEntry(add_frame, placeholder_text="Application")
-user_entry = ctk.CTkEntry(add_frame, placeholder_text="Username")
-pwd_entry = ctk.CTkEntry(add_frame, placeholder_text="Password")
+app_entry = ctk.CTkEntry(add_frame, placeholder_text="Application", text_color="white", width=300, height=50, corner_radius= 20, fg_color="#212946",border_width=1, border_color="#FFFFFF")
+user_entry = ctk.CTkEntry(add_frame, placeholder_text="Username", text_color="white", width=300, height=50, corner_radius= 20, fg_color="#212946",border_width=1, border_color="#FFFFFF")
+pwd_entry = ctk.CTkEntry(add_frame, placeholder_text="Password", text_color="white", width=300, height=50, corner_radius= 20, fg_color="#212946",border_width=1, border_color="#FFFFFF")
 
 app_entry.pack(pady=5)
 user_entry.pack(pady=5)
@@ -71,27 +79,27 @@ def save_password():
     pwd = pwd_entry.get()
 
     if not app_name or not user or not pwd:
-        add_msg.configure(text="All fields required", text_color="red")
+        add_msg.configure(text="All fields required", text_color="red", font=("Arial", 12, "bold"))
         return
 
-    encrypted = fer.encrypt(pwd.encode()).decode()
+    encrypted = fer.encrypt(pwd.encode()).decode() # Crypt saved passwords
     with open(FILE_NAME, "a") as f:
         f.write(f"{app_name}|{user}|{encrypted}\n")
 
-    add_msg.configure(text="Password saved!", text_color="green")
+    add_msg.configure(text="Password saved!", text_color="green", font=("Arial", 12, "bold"))
     app_entry.delete(0, "end")
     user_entry.delete(0, "end")
     pwd_entry.delete(0, "end")
 
-ctk.CTkButton(add_frame, text="Save", command=save_password).pack(pady=10)
-ctk.CTkButton(add_frame, text="Back", command=lambda: show_frame(menu_frame)).pack()
+ctk.CTkButton(add_frame, text="Save", command=save_password, fg_color="#FFFFFF", text_color="#141936", corner_radius=50, hover_color="#ADADAD", width=200, height=40, font=("Arial", 15, "bold")).pack(pady=10)
+ctk.CTkButton(add_frame, text="Back", command=lambda: show_frame(menu_frame), fg_color="#303030", text_color="white", corner_radius=50, hover_color="#4D4D4D", width=200, height=40, font=("Arial", 15, "bold")).pack()
 
-textbox = ctk.CTkTextbox(view_frame, width=400, height=250)
+textbox = ctk.CTkTextbox(view_frame, width=400, height=250, fg_color="#212946", text_color="white", border_width=1, border_color="#212852")
 textbox.pack(pady=20)
 
 # Load and decrypt stored passwords
 def load_passwords():
-    textbox.configure(state="normal")
+    textbox.configure(state="normal", fg_color="#212946", text_color="white", border_width=1, border_color="#212852")
     textbox.delete("1.0", "end")
 
     if not os.path.exists(FILE_NAME):
@@ -103,17 +111,16 @@ def load_passwords():
                 for line in f:
                     has_passwords = True
                     app_name, user, enc = line.strip().split("|")
-                    pwd = fer.decrypt(enc.encode()).decode()
+                    pwd = fer.decrypt(enc.encode()).decode() # Decrypt stored passwords
                     textbox.insert("end", f"Application: {app_name}\nUser: {user}\nPassword: {pwd}\n\n")
                 if not has_passwords:
                     textbox.insert("end", "Hmmm...no passwords stored.")
         except Exception as e:
             textbox.insert("end", f"An error occurred: {e}")
-
     textbox.configure(state="disabled")
 
-ctk.CTkButton(view_frame, text="Refresh", command=load_passwords).pack()
-ctk.CTkButton(view_frame, text="Back", command=lambda: show_frame(menu_frame)).pack(pady=10)
-
+ctk.CTkButton(view_frame, text="Refresh Passwords", command=load_passwords, fg_color="#FFFFFF", text_color="#141936", corner_radius=50, hover_color="#ADADAD", width=200, height=40, font=("Arial", 15, "bold")).pack()
+ctk.CTkButton(view_frame, text="Back", command=lambda: show_frame(menu_frame), fg_color="#303030", text_color="white", corner_radius=50, hover_color="#4D4D4D", width=200, height=40, font=("Arial", 15, "bold")).pack(pady=10)
+pin_entry.bind("<Return>", verify_pin) # Enter bind for Verify button
 show_frame(pin_frame)
 app.mainloop()
